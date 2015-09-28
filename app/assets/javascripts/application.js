f4a0e8a2c30e6b229d7ed911a217083d710edf8f
Dimitry Nazarov <dimitrynazarov@gmail.com>
2015-09-28 02:02:01 -0400

    jquery fiddling for dropdowns

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bootstrap
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
 //= require moment
-//= require bootstrap
 //= require bootstrap-datetimepicker
 
 $('.alert').alert();
